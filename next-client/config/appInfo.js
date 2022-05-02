0b3425ecf9b2f246a4d31e6b5f153e1af5ce99d0
souless94 <clwk94@gmail.com>
2022-05-02 21:22:20 +0800

    initial commit

---
+++ next-client/config/appInfo.js
@@ -0,0 +1,15 @@
+const port = process.env.APP_PORT || 3000;
+
+const apiBasePath = "/auth/";
+
+export const websiteDomain =
+  process.env.APP_URL ||
+  process.env.NEXT_PUBLIC_APP_URL ||
+  `http://localhost:${port}`;
+
+export const appInfo = {
+  appName: "SuperTokens Demo App",
+  websiteDomain,
+  apiDomain: "http://localhost:8000",
+  apiBasePath,
+};
---
+++ next-client/config/frontendConfig.js
@@ -0,0 +1,10 @@
+import EmailPasswordReact from "supertokens-auth-react/recipe/emailpassword";
+import SessionReact from "supertokens-auth-react/recipe/session";
+import { appInfo } from "./appInfo";
+
+export const frontendConfig = () => {
+  return {
+    appInfo,
+    recipeList: [EmailPasswordReact.init(), SessionReact.init()],
+  };
+};
